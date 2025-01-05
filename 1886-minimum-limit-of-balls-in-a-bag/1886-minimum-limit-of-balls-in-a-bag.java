fb6a05645e4eb026b1675509092c10bb58889a25
sahilsethi2 <149696800+sahilsethi2@users.noreply.github.com>
2025-01-05 16:59:54 +0530

    Time: 25 ms (98.83%), Space: 60.2 MB (66.46%) - LeetHub

--- 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
+++ 1886-minimum-limit-of-balls-in-a-bag/1886-minimum-limit-of-balls-in-a-bag.java
@@ -2,9 +2,10 @@ class Solution {
     boolean isPossible(int maxBallsInBag, int[] nums, int maxOperations) {
         int totalOperations = 0;
         for (int num : nums) {
+            // Har bag ko todne ke liye kitne operations lagenge
             totalOperations += (num - 1) / maxBallsInBag;
             if (totalOperations > maxOperations) {
-                return false;
+                return false; // Agar allowed operations se zyada ho gaye toh yeh possible nahi hai
             }
         }
         return true;
@@ -13,16 +14,17 @@ class Solution {
     public int minimumSize(int[] nums, int maxOperations) {
         int l = 1, r = 0;
         for (int num : nums) {
-            r = Math.max(r, num);
+            r = Math.max(r, num); // Maximum balls ke liye range set karo
         }
-        int ans = r;
+
+        int ans = r; // Sabse bada penalty initially
         while (l <= r) {
-            int mid = (l + r) / 2; 
+            int mid = (l + r) / 2; // Mid-point (possible penalty)
             if (isPossible(mid, nums, maxOperations)) {
-                ans = mid;
+                ans = mid; // Agar possible hai, toh penalty aur kam karne ki koshish karo
                 r = mid - 1;
             } else {
-                l = mid + 1;
+                l = mid + 1; // Agar possible nahi hai, toh penalty badhao
             }
         }
         return ans;
