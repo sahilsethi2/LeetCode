201b1228f68aeb7006259db0132e0b6b04e43e4a
sahilsethi2 <149696800+sahilsethi2@users.noreply.github.com>
2025-02-11 12:31:02 +0530

    Time: 30 ms (68.22%), Space: 51.4 MB (83.66%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,56 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        Set<List<Integer>> set = new HashSet<>();
         int n = nums.length;
-
+        Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
         for(int i=0; i<n; i++){
-            Set<Integer> hashset = new HashSet<>();
-            for(int j=i+1; j<n; j++){
-                int third = -(nums[i]+nums[j]);
+            if(i>0 && nums[i]==nums[i-1]) continue;
 
-                // Arrays.sort(nums);
-
-                if(hashset.contains(third)){
-                    List<Integer> list = Arrays.asList(nums[i],nums[j],third);
-                    Collections.sort(list);
-                    set.add(list);
+            int j = i+1;
+            int k = n-1;
+            while(j<k){
+                if(nums[i]+nums[j]+nums[k] <0){
+                    j++;
                 }
-                hashset.add(nums[j]);
-                
+                else if(nums[i]+nums[j]+nums[k] > 0){
+                    k--;
+                }
+                else{
+                    List<Integer> temp = Arrays.asList(nums[i],nums[j],nums[k]);
+                    ans.add(temp);
+                    j++;
+                    k--;
+                    while(j<k && nums[j] == nums[j-1]) j++;
+                    while(j<k && nums[k] == nums[k+1]) k--;
+                }
             }
-            
         }
-        List<List<Integer>> ans = new ArrayList<>(set);
         return ans;
-    }  
+
+
+
+        // Set<List<Integer>> set = new HashSet<>();
+        // int n = nums.length;
+
+        // for(int i=0; i<n; i++){
+        //     Set<Integer> hashset = new HashSet<>();
+        //     for(int j=i+1; j<n; j++){
+        //         int third = -(nums[i]+nums[j]);
+
+        //         // Arrays.sort(nums);
+
+        //         if(hashset.contains(third)){
+        //             List<Integer> list = Arrays.asList(nums[i],nums[j],third);
+        //             Collections.sort(list);
+        //             set.add(list);
+        //         }
+        //         hashset.add(nums[j]);
+                
+        //     }
+        // }
+        // List<List<Integer>> ans = new ArrayList<>(set);
+        // return ans;
+    
         
 
 
@@ -44,4 +71,5 @@ class Solution {
     //     List<List<Integer>> ans = new ArrayList<>();
     // }
     // return ans;
+    }
 }
