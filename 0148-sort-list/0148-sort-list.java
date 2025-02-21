700c4bf1aa2ad025eac0e7a3a4b7d2f6e7a21537
sahilsethi2 <149696800+sahilsethi2@users.noreply.github.com>
2025-02-21 12:17:34 +0530

    Time: 10 ms (46.07%), Space: 57.7 MB (6.27%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -9,25 +9,68 @@
  * }
  */
 class Solution {
+    ListNode findMiddle(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast!=null && fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    ListNode mergeTwoLists(ListNode list1, ListNode list2){
+        ListNode dummyNode = new ListNode(-1);
+
+        ListNode temp = dummyNode;
+        while(list1 != null && list2 != null){
+            if(list1.val < list2.val){
+                temp.next = list1;
+                temp = list1;
+                list1 = list1.next;
+            } 
+            else{
+                temp.next = list2;
+                temp = list2;
+                list2 = list2.next;
+            }
+        }
+        if(list1 != null) temp.next = list1;
+        else temp.next = list2;
+
+        return dummyNode.next;
+    }
     public ListNode sortList(ListNode head) {
-        List<Integer> arr = new ArrayList<>();
+        if(head==null || head.next==null) return head;
+        ListNode middle = findMiddle(head);
+        ListNode right = middle.next;
+        middle.next = null;
+        ListNode left = head;
 
-        ListNode temp = head;
+        left =  sortList(left);
+        right = sortList(right);
 
-        while(temp!=null){
-            arr.add(temp.val);
-            temp = temp.next;
-        }
+        return mergeTwoLists(left,right);
 
-        Collections.sort(arr);
-        temp = head;
 
-        int i=0;
-        while(temp!= null){
-            temp.val = arr.get(i);
-            temp = temp.next;
-            i++;
-        }
-        return head;
+
+        // List<Integer> arr = new ArrayList<>();
+
+        // ListNode temp = head;
+
+        // while(temp!=null){
+        //     arr.add(temp.val);
+        //     temp = temp.next;
+        // }
+
+        // Collections.sort(arr);
+        // temp = head;
+
+        // int i=0;
+        // while(temp!= null){
+        //     temp.val = arr.get(i);
+        //     temp = temp.next;
+        //     i++;
+        // }
+        // return head;
     }
 }
