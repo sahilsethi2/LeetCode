89b1a397f23745b8fead1a8d184f5487ae079b71
sahilsethi2 <149696800+sahilsethi2@users.noreply.github.com>
2025-02-21 11:29:54 +0530

    Time: 0 ms (100%), Space: 44.4 MB (67.53%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -15,20 +15,61 @@ class Node {
 
 class Solution {
     public Node copyRandomList(Node head) {
+
         Node temp = head;
-        HashMap<Node, Node> map = new HashMap<>();
-        while (temp != null){
-            Node newNode = new Node(temp.val);
-            map.put(temp, newNode);
-            temp = temp.next;
+        while(temp!=null){
+            Node copyNode = new Node(temp.val);
+            copyNode.next = temp.next;
+            temp.next = copyNode;
+            temp = temp.next.next;
         }
+
         temp = head;
-        while (temp != null){
-            Node copyNode = map.get(temp);
-            copyNode.next = map.get(temp.next);
-            copyNode.random = map.get(temp.random);
+        while(temp!=null){
+            if(temp.random != null){
+                temp.next.random = temp.random.next;
+            }
+            else{
+                temp.next.random = null;
+            }
+            temp = temp.next.next;
+        }
+
+        Node dummyNode = new Node(-1);
+        Node res = dummyNode;
+
+        temp = head;
+        while(temp!=null){
+            res.next = temp.next;
+            temp.next = temp.next.next;
+            res = res.next;
             temp = temp.next;
         }
-        return map.get(head);
+
+        return dummyNode.next;
+
+
+
+
+
+
+
+
+
+        // Node temp = head;
+        // HashMap<Node, Node> map = new HashMap<>();
+        // while (temp != null){
+        //     Node newNode = new Node(temp.val);
+        //     map.put(temp, newNode);
+        //     temp = temp.next;
+        // }
+        // temp = head;
+        // while (temp != null){
+        //     Node copyNode = map.get(temp);
+        //     copyNode.next = map.get(temp.next);
+        //     copyNode.random = map.get(temp.random);
+        //     temp = temp.next;
+        // }
+        // return map.get(head);
     }
 }
