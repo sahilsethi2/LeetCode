a9368338c733321942fce801e756731af60cf310
sahilsethi2 <149696800+sahilsethi2@users.noreply.github.com>
2025-03-02 22:18:02 +0530

    Time: 0 ms (100%), Space: 42.6 MB (20.63%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0, count = 0;
+        for(int i=0; i<nums.length; i++){
+            sum += nums[i];
+            if(sum == 0) count++;
+        }
+        return count;
+    }
+}
